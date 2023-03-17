cfedd106b1592abd85231efee6f1fa4b0c14c692
河野流星 <ryusei@kounoryuuseinoMacBook-Pro.local>
2023-03-17 13:05:24 +0900

    css-firebase追加

--- src/views/MemoList.vue
+++ src/views/MemoList.vue
@@ -18,6 +18,7 @@
 
 <style scoped>
 
+/* デスクトップ用スタイル */
 ul {
   margin-right: 300px;
   margin-left: 300px;
@@ -35,6 +36,53 @@ li a {
   text-decoration: none;
   width: 100%;
   display: block;
+}
+
+@media screen and (max-width: 1023px) {
+  /* タブレット・スマートフォン用スタイル */
+  ul {
+    margin-right: 20px;
+    margin-left: 20px;
+  }
+
+  li {
+    padding-bottom: 5px;
+    margin-bottom: 5px;
+  }
+}
+
+/* タブレット・スマートフォン用スタイル */
+ul {
+  margin-right: 20px;
+  margin-left: 20px;
+  padding-left: 0;
+}
+
+li {
+  list-style: none;
+  border-bottom:  3px solid #ccc;
+  padding-bottom: 5px;
+  margin-bottom: 5px;
+}
+
+li a {
+  color: #999;
+  text-decoration: none;
+  width: 100%;
+  display: block;
+}
+
+@media screen and (min-width: 1024px) {
+  /* デスクトップ用スタイル */
+  ul {
+    margin-right: 300px;
+    margin-left: 300px;
+  }
+
+  li {
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+  }
 }
 </style>
 
